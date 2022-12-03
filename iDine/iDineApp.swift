c42a4bce3ce6b166d9c58879ec27583cb76f3864
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-12-03 12:19:37 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Vithushan Logan on 2022-12-03.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
